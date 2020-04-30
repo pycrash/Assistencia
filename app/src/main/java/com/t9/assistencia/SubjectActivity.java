d35a6b366ea36ab232fcb05053e767401be0a16a
pycrash <ashutoshjha2285@gmail.com>
2020-04-30 20:58:15 +0530

    Updated

--- app/src/main/java/com/t9/assistencia/SubjectActivity.java
+++ app/src/main/java/com/t9/assistencia/SubjectActivity.java
@@ -1,22 +1,20 @@
 package com.t9.assistencia;
 
-import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 import androidx.core.content.ContextCompat;
 
 import android.content.Context;
-import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
@@ -24,6 +22,7 @@ import android.widget.Toast;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import java.sql.Time;
 import java.util.ArrayList;
 
 public class SubjectActivity extends AppCompatActivity {
@@ -40,18 +39,18 @@ public class SubjectActivity extends AppCompatActivity {
     SharedPreferences sharedpreferences;
     public static final String MyPREFERENCES = "MyChoice" ;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_subject);
 
         rootLayout=(RelativeLayout)findViewById(R.id.root_layout_subjects);
         //Creating the instance of ArrayAdapter containing list of fruit names
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>
+        final ArrayAdapter<String> suggestionAdapter = new ArrayAdapter<String>
                 (this, android.R.layout.simple_spinner_dropdown_item, fruits);
         //Getting the instance of AutoCompleteTextView
         actv = (AutoCompleteTextView) findViewById(R.id.auto_complete_subjects);
         actv.setThreshold(1);//will start working from first character
-        actv.setAdapter(adapter);//setting the adapter data into the AutoCompleteTextView
+        actv.setAdapter(suggestionAdapter);//setting the adapter data into the AutoCompleteTextView
         actv.setTextColor(ContextCompat.getColor(SubjectActivity.this, android.R.color.white));
         actv.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,35 +58,66 @@ public class SubjectActivity extends AppCompatActivity {
                 actv.showDropDown();
             }
         });
-        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                isSelectedText = true;
-            }
-        });
+
 
         simpleList=(ListView)findViewById(R.id.list_subjects);
         specialitiesSearch=new ArrayList<>();
-        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(SubjectActivity.this,R.layout.list_item_size_12sp,R.id.textView, specialitiesSearch);
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(SubjectActivity.this,R.layout.list_item_subject,R.id.textView, specialitiesSearch);
 
         View.OnClickListener onClickListener=new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                    String s = actv.getEditableText().toString();
-                    if(!specialitiesSearch.contains(s)){
+                String s = actv.getEditableText().toString();
+                if (s.equals("")) {
+                    Snackbar.make(rootLayout, "Enter a Subject", Snackbar.LENGTH_LONG).show();
+
+                } else {
+                    if (!specialitiesSearch.contains(s)) {
                         specialitiesSearch.add(s);
                         actv.setText("");
                         arrayAdapter.notifyDataSetChanged();
-                    }
-                    else{
-                        Snackbar.make(rootLayout,"Subject Already exists",Snackbar.LENGTH_LONG).show();
+                    } else {
+                        Snackbar.make(rootLayout, "Subject Already exists", Snackbar.LENGTH_LONG).show();
                     }
                 }
+            }
         };
+        actv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String s = actv.getEditableText().toString();
+
+                if(!specialitiesSearch.contains(s)){
+                    specialitiesSearch.add(s);
+                    actv.setText("");
+                    arrayAdapter.notifyDataSetChanged();
+                }
+                else{
+                    Snackbar.make(rootLayout,"Subject Already exists",Snackbar.LENGTH_LONG).show();
+                }
+                }
+
+        });
         add=(CardView)findViewById(R.id.add_subjects);
         add.setOnClickListener(onClickListener);
         simpleList.setAdapter(arrayAdapter);
+        AdapterView.OnItemClickListener itemClickListener=new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view, final int position, long id) {
+                ImageView delete=(ImageView)findViewById(R.id.delete_subjects);
+                delete.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        arrayAdapter.remove(specialitiesSearch.remove(position));
+                        simpleList.requestLayout();
+                        arrayAdapter.notifyDataSetChanged();
+
+                    }
+                });
+            }
+        };
+        simpleList.setOnItemClickListener(itemClickListener);
 
 
 
@@ -108,9 +138,14 @@ public class SubjectActivity extends AppCompatActivity {
                 }
 
                 Toast.makeText(SubjectActivity.this, selected, Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(SubjectActivity.this, TimetableActivity.class);
+                TinyDB tinydb = new TinyDB(SubjectActivity.this);
+                tinydb.putListString("Subjects", specialitiesSearch);
+                startActivity(intent);
 
             }});
     }
+
 
 }
 
--- app/src/main/java/com/t9/assistencia/SundayFragment.java
+++ app/src/main/java/com/t9/assistencia/SundayFragment.java
@@ -1,12 +1,23 @@
 package com.t9.assistencia;
 
+import androidx.annotation.BinderThread;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+
+import java.util.ArrayList;
 
 public class SundayFragment extends Fragment {
 
@@ -14,11 +25,74 @@ public class SundayFragment extends Fragment {
         // Required empty public constructor
     }
 
+    CardView btnBottomSheet;
+    ListView listView;
+    ArrayList<String> subjects;
+
+    LinearLayout layoutBottomSheet;
+
+    BottomSheetBehavior sheetBehavior;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View rootView = inflater.inflate(R.layout.activity_monday_fragment, container, false);
+        View rootView = inflater.inflate(R.layout.activity_sunday_fragment, container, false);
+        TinyDB tinydb = new TinyDB(getContext());
+        subjects=tinydb.getListString("Subjects");
 
+        layoutBottomSheet=(LinearLayout)rootView.findViewById(R.id.bottom_sheet);
+        listView=(ListView)rootView.findViewById(R.id.subjects_in_timetable);
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(rootView.getContext(), android.R.layout.simple_list_item_1, subjects);
+        listView.setAdapter(arrayAdapter);
+
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                        break;
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+        sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+        btnBottomSheet=(CardView) rootView.findViewById(R.id.btn_bottom_sheet);
+        btnBottomSheet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toggleBottomSheet();
+            }
+        });
         return rootView;
     }
+    public void toggleBottomSheet() {
+        if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        }
+    }
 }
+
+
+
